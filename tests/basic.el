bed9425f4cc67f78a5990cde641ff526a55ce668
Kazure Zheng <kazure@debian>
2026-02-17 11:58:20 +0800

    add a basic test

---
+++ tests/basic.el
@@ -0,0 +1,13 @@
+(require 'ert)
+(require 'kindle-mate)
+
+(ert-deftest kindle-mate-resets-parameters-test ()
+  (let ((kindle-mate-full-clippings "x")
+        (kindle-mate-book-name "y")
+        (kindle-mate-book-log "z")
+        (kindle-mate-book-notes "w"))
+    (kindle-mate-resets-parameters)
+    (should (equal kindle-mate-full-clippings ""))
+    (should (equal kindle-mate-book-name ""))
+    (should (equal kindle-mate-book-log ""))
+    (should (equal kindle-mate-book-notes ""))))
