93d90bc9979c813c8a81c2f9f55b8609a91c41ce
Kazure Zheng <kazure@debian>
2026-02-17 11:34:12 +0800

    set up corresponding information

---
+++ kindle-mate.el
@@ -0,0 +1,18 @@
+;;; kindle-mate.el --- Short description -*- lexical-binding: t; -*-
+
+;; Author: Kazure Zheng <kazurezheng@gmail.com>
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "26.1"))
+;; Keywords: kindle
+;; URL: https://github.com/zkazure/kindle-mate.git
+
+;;; Commentary:
+;; read "My Clippings.txt" from kindle device,
+;; search for corresponding notes with fuzzy-book-name,
+;; then output the book-notes
+
+;;; Code:
+
+
+(provide 'kindle-mate)
+;;; kindle-mate.el ends here
